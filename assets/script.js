5a4995fab27bc1423658b81c81484e69412cf505
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-03-28 14:33:31 -0400

    added the start page to the html and css

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+        question:`What does CSS stand for?`,
+        options:[`Cat Soul Snatcher`, `Carmen Sandigeo Steals`, `Can't Stop Sam`,`Cascading Style Sheet`],
+        answer:3
+    },
+    {
+        question: `What is my favourite fruit?`,
+        options:[`pomegranete`, `clemintines`, `grapes`, `cherries`],
+        answer:0
+    },
+    {
+        question: `What season is the coldest?`,
+        options:[`winter`,`summer`, `spring`, `fall`],
+        answer:0
+    },
+    {
+        question:`What is not a vegetable?`,
+        options:[`apple`, `carrot`, `grape`, `strawberry`],
+        answer:1
+    },
+    {
+        question:`How many month in a year?`,
+        options:[`12`,`13`,`14`,`15`],
+        answer: 0
+    },
+    console.log(questions)
+]
