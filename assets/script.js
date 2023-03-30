76f4509fd895f22b4f37e2596012589e4c2c8277
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-03-30 13:32:08 -0400

    created a function that will move from homepage to questions

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,37 @@
-var questionContainer = document.querySelector(".question-container")
+//triggering the quiz to start
+var startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startQuiz)
+//Added so the home page will disappear and questions will appear in the function startQuiz
+var homePage = document.getElementById('starter-page')
+var questionContainer = document.getElementById('question-container')
 
-var questions = [
+
+function startQuiz () {
+    console.log('Start')
+    homePage.classList.add('hide'),
+    console.log('clear')
+    questionContainer.classList.remove('hide')
+}
+
+var question = document.getElementById('question-text')
+var options = document.getElementById('option-container')
+var answer = document.getElementById('answer-display')
+
+
+var questionContainer = [
     {
-        question:'What month comes after April?',
-        options:['May','June', 'July','August'],
+        question:'What is contained in []?',
+        options:['array','boolean', 'string','numbers'],
         answer:0
     },
     {
-        question: 'What month comes after March?',
-        options:['April','May','June','July'],
+        question: 'A usful tool for debugging is:____.',
+        options:['console.log','CSS','alerts','booleans'],
         answer:0
     },
     {
-        question: 'What month comes after May?',
-        options:['June','July','August','September'],
+        question: 'Commolnly used data types DO NOT include____.',
+        options:['alert','boolean','string','array'],
         answer:0
     },
-    console.log(questions)
 ]
